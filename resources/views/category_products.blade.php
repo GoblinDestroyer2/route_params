d2c1066aa5b2356041bb51725c29c69852d0ee99
GoblinDestroyer2 <ps228991@summacollege.nl>
2025-09-19 11:24:38 +0200

    Initial commit

---
+++ resources/views/category_products.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Producten - {{ $category }}</title>
+</head>
+<body>
+    <h1>Producten voor categorie: {{ $category }}</h1>
+    <a href="/">Terug naar home</a>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Categorieën</title>
+</head>
+<body>
+    <h1>Onze productcategorieën</h1>
+    <ul>
+        <li><a href="/categories/tablets/products">Tablets</a></li>
+        <li><a href="/categories/laptops/products">Laptops</a></li>
+        <li><a href="/categories/desktops/products">Desktops</a></li>
+        <li><a href="/categories/smartphones/products">Smartphones</a></li>
+    </ul>
+</body>
+</html>
